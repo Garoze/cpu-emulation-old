7be4019ca324fafe206250698923f0551bcc74e0
Garoze <marcelogaroze@hotmail.com>
2022-02-05 20:26:30 -0300

    initial commit

---
+++ configure.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CHECK_FOLDERS='build'
+
+for folder in $(echo $CHECK_FOLDERS | sed "s/,/ /g"); do
+    if [[ ! -d $folder ]]; then
+        echo -e "Creating $folder..."; mkdir $folder
+    fi
+done
+
+make
