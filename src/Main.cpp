ca8507d65e149fbc43515ca218ad5a3db170b03b
Garoze <marcelogaroze@hotmail.com>
2022-02-05 22:41:42 -0300

    feat: Main.cpp
    Tested the CPU with simples instructions and it worked (for now)

---
+++ src/Main.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <cstdio>
+
+#include "include/CPU/CPU.hpp"
+
+using i = Instructions;
+
+int main()
+{
+    CPU cpu;
+
+    const std::vector<std::uint16_t> code = {
+        (int)i::NOP,
+        (int)i::PSH, 5,
+        (int)i::POP,
+        (int)i::HTL,
+    };
+    cpu.loadToMemory(code);
+    cpu.run();
+
+    return 0;
+}
